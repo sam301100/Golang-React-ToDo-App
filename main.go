d7150f546c698a3b4f536ff60e397db63d570086
Samruddhi <samruddhisomwanshi30@gmail.com>
2023-10-18 15:48:10 -0700

    1/4th project done

--- main.go
+++ main.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"fmt"
-	"golang-react-todo/router"
 	"log"
 	"net/http"
+
+	"github.com/sam301100/golang-react-todo/router"
 )
 
 func main() {
 	r := router.Router()
-	fmt.Println("Starting the server on port 9000...")
+	fmt.Println("starting the server on port 9000...")
 
 	log.Fatal(http.ListenAndServe(":9000", r))
 }
--- server/models/models.go
+++ server/models/models.go
@@ -4,7 +4,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 //struct is our own datatype created in golang
 type ToDoList struct {
-	ID     primitive.ObjectID `json: "_id,omitempty bson:"_id, omitempty"`
-	Task   string             `json: "task,omitempty"`
-	Status bool               `json: "status,omitempty"`
+	ID     primitive.ObjectID `json:"_id,omitempty"  bson:"_id,omitempty"`
+	Task   string             `json:"task,omitempty"`
+	Status bool               `json:"status, omitempty"`
 }
